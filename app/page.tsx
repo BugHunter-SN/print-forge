2afe0c9993dec97e89c3dcd27d2ce6c9f98df181
Nimely111 <samuelehnimely@gmail.com>
2025-06-14 18:01:03 +0000

    add the about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import HeroImage from '../public/Frame 4.png'
+import HeroImage from '../public/hero-image.png'
 import Button from "./components/button"
 export default function Home() {
   return (
