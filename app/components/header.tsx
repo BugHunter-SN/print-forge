16ea46e291fd79692ed4d7bb22c256e149b0b385
Nimely111 <samuelehnimely@gmail.com>
2025-06-19 02:41:34 +0000

    feat: surrend the div containing the images with the link component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,27 +7,25 @@ export default function Header() {
   return (
  <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
+            <Link href='/'>
             <div className="relative">
               {/* Desktop logo */}
-               <Link href='/'>
                 <Image
                 src={DesktopLogo}
                 alt='Printforge logo'
                 className="w-[200px] h-auto hidden md:block"
                />
-               </Link>
               {/* Mobile logo */}
-             <Link href='/'>
                <Image
                 src={MobileLogo}
                 alt='Printforge logo'
                 className="w-[40px] h-auto block md:hidden"
                />
-             </Link>
             </div>
+             </Link>
             <ul className="flex items-center gap-2.5">
               <li>
-                <Link href='/'>3D Models</Link>
+                <Link href='/3d-models'>3D Models</Link>
               </li>
               <li>
                 <Link href='/about'>About</Link>
