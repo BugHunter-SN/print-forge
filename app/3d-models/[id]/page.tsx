b8a0d5787a50104840a83455f764bc6d59982a07
Nimely111 <samuelehnimely@gmail.com>
2025-06-21 23:25:39 +0000

    update the 3d-models and delete the post directory

--- app/3d-models/[id]/page.tsx
+++ app/3d-models/[id]/page.tsx
@@ -1,7 +1,10 @@
-export default function ModelDetail() {
-  return (
-    <div>
-      
-    </div>
-  )
+import type { ModelDetailPageProps } from "@/app/types"
+import { getModelById } from "@/app/lib/models"
+import { log } from "console"
+export default async function ModelDetailPage({ params }: ModelDetailPageProps) {
+    const { id } = await params
+    const model = await getModelById(id)
+    console.log(model);
+
+  return <h1>The id of the model is {id}</h1>
 }
