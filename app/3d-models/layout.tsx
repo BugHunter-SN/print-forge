0fb96fe5c93b3d78c41a64789b12b82d203f39d5
Nimely111 <samuelehnimely@gmail.com>
2025-07-23 11:42:42 +0000

    feat: conditionally render all category links to redirect to the rightful category

--- app/3d-models/layout.tsx
+++ app/3d-models/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import React from 'react'
 import type { ReactNode } from "react"
 import { getAllCategories } from "@/app/lib/categories"
@@ -6,7 +7,15 @@ export default function ModelsLayout({children} : {children: ReactNode}) {
     console.log(categories);
   return (
     <div>
-        <h1>Navbar will go here</h1>
+        <nav>
+            <Link href="/3d-models">All</Link>
+            {categories.map(cat =>(
+                <Link 
+                    href={`/3d-models/categories/${cat.slug}`}
+                    key={cat.slug}>{cat.displayName}
+                </Link>
+            ))}
+        </nav>
         {children}
     </div>
   )
