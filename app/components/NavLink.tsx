00186a69ed2b87e0460ff5887f173f67ef2cc0f6
Nimely111 <samuelehnimely@gmail.com>
2025-07-23 13:47:06 +0000

    feat: add hover effect on sub-menu under 3D-models path

--- app/components/NavLink.tsx
+++ app/components/NavLink.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link"
-import { ReactNode } from "react"
 import type { NavLinkProps } from "@/app/types"
 
 export default function({href, children} : NavLinkProps){
-
+    return(
+        <li className="text-sm uppercase">
+            <Link 
+            href={href}
+            className="px-4 py-2 text-gray-700 transition-colors rounded-md cursor-pointer hover:text-orange-accent">
+            {children}
+            </Link>
+        </li>
+    )
 }
