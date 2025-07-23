9e2337ebb32d8e8a45ad4b4d1819cb48a1f40738
Nimely111 <samuelehnimely@gmail.com>
2025-07-23 09:57:50 +0000

    fix: fixed import typo and also import the get category by slug

--- app/3d-models/categories/[categoryName]/page.tsx
+++ app/3d-models/categories/[categoryName]/page.tsx
@@ -1,5 +1,5 @@
-import type { CategoryPageProps } from "@/types"
-
+import type { CategoryPageProps } from "@/app/types/index"
+import { getCategoryBySlug } from "@/app/lib/categories"
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { categoryName } = params; // no need for await
   return <h1>{categoryName}</h1>
